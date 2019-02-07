f237e3d2cfe7ae31e607a6b1f20010607693dfab
godfreyJo <godfreyoyugi@gmail.com>
2019-02-07 13:05:28 +0300

    addition of all django apps

--- hellobooks/settings.py
+++ hellobooks/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'administrators.apps.AdministratorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
